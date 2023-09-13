9a611397b3e13924d2377c2b42cc8bd2a459b600
maximo cavero <cavero.maximo.l@gmail.com>
2023-09-12 23:59:52 -0300

    agregando proyecto

---
+++ adopta-un-perrito/src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>APP</h1>
+    </>
+  )
+}
+
+export default App
---
+++ adopta-un-perrito/src/components/Tags.jsx
@@ -0,0 +1,5 @@
+export const Tags = () => {
+  return (
+    <div>Tags</div>
+  )
+}
